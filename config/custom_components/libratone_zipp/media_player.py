164b11f89eb986e1cfa2b33ef308f55f93777fec
Chouffy <git@chouffy.net>
2021-06-15 18:15:26 +0200

    🥳 Now with status updates from the Zipp!

--- config/custom_components/libratone_zipp/media_player.py
+++ config/custom_components/libratone_zipp/media_player.py
@@ -10,13 +10,17 @@ from homeassistant.components.media_player.const import (
     SUPPORT_STOP,
     SUPPORT_NEXT_TRACK,
     SUPPORT_PREVIOUS_TRACK,
+    SUPPORT_SELECT_SOURCE,
     SUPPORT_SELECT_SOUND_MODE,
+    SUPPORT_TURN_OFF,
+    SUPPORT_TURN_ON,
+    SUPPORT_VOLUME_SET,
+    SUPPORT_VOLUME_STEP,
 )
 from homeassistant.const import (
     CONF_HOST,
     CONF_NAME,
     STATE_IDLE,
-    STATE_ON,
     STATE_OFF,
     STATE_PLAYING,
     STATE_PAUSED,
@@ -36,6 +40,11 @@ SUPPORT_LIBRATONE_ZIPP = (
     | SUPPORT_NEXT_TRACK
     | SUPPORT_PREVIOUS_TRACK
     | SUPPORT_SELECT_SOUND_MODE
+    | SUPPORT_SELECT_SOURCE
+    | SUPPORT_TURN_OFF
+    | SUPPORT_TURN_ON
+    | SUPPORT_VOLUME_SET
+    | SUPPORT_VOLUME_STEP
 )
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
@@ -54,31 +63,55 @@ def setup_platform(hass, config, add_entities, discover_info=None):
     host = config[CONF_HOST]
     name = config[CONF_NAME]
 
-    zipp = LibratoneZipp(host, name)
-    zipp_device = LibratoneZippDevice(zipp)
+    zipp_client = LibratoneZipp(host, name)
 
-    add_entities([zipp_device], True)
+    add_entities([LibratoneZippDevice(zipp_client)])
 
 
 class LibratoneZippDevice(MediaPlayerEntity):
     """Representation of a Libratone Zipp speaker."""
 
-    def __init__(self, zipp):
+    def __init__(self, zipp_client):
         """Initialize a new Libratone Zipp device"""
-        self._zipp = zipp
+        self._zipp_client = zipp_client
 
-        self._name = zipp.name
-        self._host = zipp.host
+        self._name = self._zipp_client.name
+        self._host = self._zipp_client.host
+
+        self._device_type = DEVICE_CLASS_SPEAKER
+
+        self._state = None
+        self._volume_level = None
+
+        # self._source = None
+        self._source_list = ["1", "2", "3", "4", "5"]
 
         self._sound_mode = None
-        self._sound_mode_list = zipp.voicing_list
+        self._sound_mode_list = self._zipp_client.voicing_list
 
-        self._state = STATE_ON
-        self._device_type = DEVICE_CLASS_SPEAKER
+        self.update()
 
     def update(self):
         """Update the state of this media_player."""
-        self._sound_mode = self._zipp.voicing
+
+        # Update of state
+        if self._zipp_client.state == "OFF":
+            self._state = STATE_OFF
+        elif self._zipp_client.state == "PLAY":
+            self._state = STATE_PLAYING
+        elif self._zipp_client.state == "PAUSE":
+            self._state = STATE_PAUSED
+        elif self._zipp_client.state == "STOP":
+            self._state = STATE_IDLE
+        else:
+            self._state = STATE_UNKNOWN
+
+        # Update of volume
+        if self._zipp_client.volume != None and self._zipp_client.volume != "":
+            self._volume_level = int(self._zipp_client.volume) / 100
+
+        # Update of sound mode
+        self._sound_mode = self._zipp_client.voicing
 
     @property
     def name(self):
@@ -91,6 +124,11 @@ class LibratoneZippDevice(MediaPlayerEntity):
         return self._state
 
     @property
+    def volume_level(self):
+        """Volume level of the media player (0..1)."""
+        return self._volume_level
+
+    @property
     def supported_features(self):
         """Flag media player features that are supported."""
         return SUPPORT_LIBRATONE_ZIPP
@@ -99,6 +137,18 @@ class LibratoneZippDevice(MediaPlayerEntity):
     def media_content_type(self):
         """Content type of current playing media."""
         return MEDIA_TYPE_MUSIC
+
+    '''
+    @property
+    def source(self):
+        """Name of the current input source."""
+        return None
+    '''
+
+    @property
+    def source_list(self):
+        """List of available input sources."""
+        return self._source_list
 
     @property
     def sound_mode(self):
@@ -110,34 +160,44 @@ class LibratoneZippDevice(MediaPlayerEntity):
         """Return the current sound mode."""
         return self._sound_mode_list
 
+    def turn_on(self):
+        """Turn the media player on."""
+        return self._zipp_client.wakeup()
+
+    def turn_off(self):
+        """Turn the media player off."""
+        return self._zipp_client.sleep()
+
+    def set_volume_level(self, volume):
+        """Set volume level, range 0..1."""
+        # In order to get a smooth volume slider, volume is overriden here - but it will be updated later anyway
+        self._volume_level = volume
+        return self._zipp_client.volume_set(int(volume * 100))
+
     def media_play(self):
         """Send play command."""
-        self._state = STATE_PLAYING
-        return self._zipp.play()
+        return self._zipp_client.play()
 
     def media_pause(self):
         """Send pause command."""
-        self._state = STATE_PAUSED
-        return self._zipp.pause()
+        return self._zipp_client.pause()
 
     def media_stop(self):
         """Send stop command."""
-        self._state = STATE_IDLE
-        return self._zipp.stop()
+        return self._zipp_client.stop()
 
     def media_next_track(self):
         """Send next command."""
-        self._state = STATE_PLAYING
-        return self._zipp.next()
+        return self._zipp_client.next()
 
     def media_previous_track(self):
         """Send prev command."""
-        self._state = STATE_PLAYING
-        return self._zipp.prev()
+        return self._zipp_client.prev()
+
+    def select_source(self, source):
+        """Select input source."""
+        return self._zipp_client.favorite_play(source)
 
     def select_sound_mode(self, sound_mode):
-        """Set Sound Mode for Receiver.."""
-        self._zipp.voicing_set(sound_mode)
-        self._sound_mode = sound_mode
-        return True
-        
+        """ "Select sound mode."""
+        return self._zipp_client.voicing_set(sound_mode)
